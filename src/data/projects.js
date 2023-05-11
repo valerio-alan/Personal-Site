48a7a54c4dbdc275ac312f4c7d9b78f942dc8300
FlatTireTwitch <alan@valerio.nyc>
2023-05-10 21:03:48 -0400

    Added more projects for more accurate preview

--- src/data/projects.js
+++ src/data/projects.js
@@ -29,7 +29,67 @@ export default {
                 backgroundColor: '115, 74, 74',
                 textColor: '239, 231, 231'
             }
-        }
+        },
+        {
+            title: 'Franja Liquors',
+            description: "In addition to designing and developing this website for a liquor store in Ridgewood, NY. This project included creating an iOS shortcut that allows the business owner to update the store's hours and site banner from his iPhone.",
+            links: {
+                production: 'https://franjaliquors.com',
+                github: 'https://github.com/valerio-alan/Franja-Site'
+            },
+            image: 'franja-liquors.png',
+            imageAlt: 'Franja Liquors Website Screenshot',
+            tags: ['html/css/js', 'python', 'netlify', 'deta-space'],
+            colors: {
+                backgroundColor: '49, 50, 57',
+                textColor: '230, 225, 218'
+            }
+        },
+        {
+            title: "Jane Casto",
+            description: "This is the portfolio site I designed and developed for Jane Casto. Jane is a policy and data analyst and this simple site showcases her work and provides a way for potential clients to contact her.",
+            links: {
+                production: 'https://janecasto.com',
+                github: 'https://github.com/valerio-alan/Jane-Casto-Portfolio'
+            },
+            image: 'jane-casto.png',
+            imageAlt: "Jane Casto's Portfolio Website Screenshot",
+            tags: ['react', 'netlify', 'figma'],
+            colors: {
+                backgroundColor: '115, 74, 74',
+                textColor: '239, 231, 231'
+            }
+        },
+        {
+            title: 'Franja Liquors',
+            description: "In addition to designing and developing this website for a liquor store in Ridgewood, NY. This project included creating an iOS shortcut that allows the business owner to update the store's hours and site banner from his iPhone.",
+            links: {
+                production: 'https://franjaliquors.com',
+                github: 'https://github.com/valerio-alan/Franja-Site'
+            },
+            image: 'franja-liquors.png',
+            imageAlt: 'Franja Liquors Website Screenshot',
+            tags: ['html/css/js', 'python', 'netlify', 'deta-space'],
+            colors: {
+                backgroundColor: '49, 50, 57',
+                textColor: '230, 225, 218'
+            }
+        },
+        {
+            title: "Jane Casto",
+            description: "This is the portfolio site I designed and developed for Jane Casto. Jane is a policy and data analyst and this simple site showcases her work and provides a way for potential clients to contact her.",
+            links: {
+                production: 'https://janecasto.com',
+                github: 'https://github.com/valerio-alan/Jane-Casto-Portfolio'
+            },
+            image: 'jane-casto.png',
+            imageAlt: "Jane Casto's Portfolio Website Screenshot",
+            tags: ['react', 'netlify', 'figma'],
+            colors: {
+                backgroundColor: '115, 74, 74',
+                textColor: '239, 231, 231'
+            }
+        },
     ],
     tags: {
         first_project: {
